8bb278c4645c4b1d8874b107b0dcf64a8a7ace3d
OptiLab ROA <icortes@roa.es>
2025-12-09 08:33:54 +0100

    Remove Conda environment installation script

--- install_conda.bat
+++ install_conda.bat
@@ -1,18 +1,1 @@
-@echo off
-echo --- Creating Conda Environment ---
 
-:: Check if Conda is available
-conda --version >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Error: Conda is not found. Please open this file using Anaconda Prompt.
-    pause
-    exit /b
-)
-
-:: Create environment from YAML file
-echo Creating environment from ROA_DOZE.yml...
-call conda env create -f ROA_DOZE.yml
-
-echo.
-echo --- Installation Finished ---
-pause
