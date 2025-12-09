3915d8f8dbdef464536decb8452ebe7f10c9f581
OptiLab ROA <icortes@roa.es>
2025-12-09 08:35:53 +0100

    Remove run_conda.bat script

--- run_conda.bat
+++ run_conda.bat
@@ -1,19 +1,1 @@
-@echo off
-echo --- Starting the Application ---
 
-set ENV_NAME=ROA_DOZE
-
-:: Activate Conda environment
-:: We use 'call conda' to ensure the batch script continues after activation
-call conda activate %ROA_DOZE%
-
-if %errorlevel% neq 0 (
-    echo Error: Could not activate environment '%ROA_DOZE%'.
-    pause
-    exit /b
-)
-
-:: Run the script
-python ZeemanAPP.py
-
-pause
